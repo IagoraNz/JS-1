44412e95914d45f94264a3bc2cb78a92a33b807e
IagoraNz <iago.almeida@ufpi.edu.br>
2023-11-15 15:16:41 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-var dolar = 30, real = 5.32; // Real em relação ao Dólar
+var dolar = 4.86, euro = 5.27;
 var res;
 
-var moeda = prompt("Qual moeda você quer converter? \n 1 - Dólar para Real\n 2 - Real para Dólar");
+do{
+    var moeda = prompt("Qual moeda você quer converter? \n 1 - Dólar para Real\n 2 - Real para Dólar\n 3 - Euro para Real\n 4 - Real para Euro");  
+}while(moeda < 1 || moeda > 6) //Para garantir que seja escolhido uma opção válida
+
+var valor = parseFloat(prompt("Digite o valor: "));
 
 function conversao(moeda){
 
     if(moeda == 1){
-        res = dolar / real;
+        res = valor * dolar;
+        alert("O valor em dólar é: " + res.toFixed(2));
     }
     else if(moeda == 2){
-        res = dolar * real;
+        res = valor / dolar;
+        alert("O valor em real é: " + res.toFixed(2));
+    }
+    else if(moeda == 3){
+        res = euro * valor;
+        alert("O valor em real é: " + res.toFixed(2));
+    }
+    else if(moeda == 4){
+        res = valor / euro;
+        alert("O valor em euro é: " + res.toFixed(2));
     }
     else{
         alert('Invalido');
@@ -18,6 +32,4 @@ function conversao(moeda){
     return res;
 }
 
-var resultado = conversao(moeda);
-
-alert('Valor: ' + resultado.toFixed(2));
+conversao(moeda);
