89b15743a4a1b26c5348e7ddfd255cdd210c4869
IagoraNz <iago.almeida@ufpi.edu.br>
2023-11-15 14:24:51 -0300

    Save

---
+++ script.js
@@ -0,0 +1,23 @@
+var dolar = 30, real = 5.32; // Real em relação ao Dólar
+var res;
+
+var moeda = prompt("Qual moeda você quer converter? \n 1 - Dólar para Real\n 2 - Real para Dólar");
+
+function conversao(moeda){
+
+    if(moeda == 1){
+        res = dolar / real;
+    }
+    else if(moeda == 2){
+        res = dolar * real;
+    }
+    else{
+        alert('Invalido');
+    }
+
+    return res;
+}
+
+conversao(moeda);
+
+alert(res);
